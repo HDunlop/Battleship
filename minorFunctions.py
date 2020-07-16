e019e21dec8e867fc2fcf311e4939d3660fc15c1
HDunlop <dunlop.h@etoncollege.org.uk>
2020-07-16 17:01:42 +0800

    minorFunctions.py v0

---
+++ minorFunctions.py
@@ -0,0 +1,42 @@
+from termcolor import colored
+
+def create2DListOfMoves():
+  permValues = ["0 ","A","B","C","D","E","F","G","H","I","J","1 ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10",(5,5),(5,6),(6,5),(6,6),(1,1),(10,10),(1,10),(10,1)]
+  letters = ["A","B","C","D","E","F","G","H","I","J"]
+  moves = []
+  for i in range(11):
+    temp = []
+    for j in range(11):
+      toAdd = "0"
+      if (j == 0) and (i != 0):
+        if i == 10:
+          toAdd = str(i)
+        else:
+          toAdd = str(i) + " "
+      if (len(letters) > 0) and (i == 0) and (j != 0):
+        toAdd = letters.pop(0)
+      if (i == 0) and (j == 0):
+        toAdd = "0 "
+      temp.append(toAdd)
+    moves.append(temp)
+  return moves,permValues
+
+def printMoves(copy1):
+  for row in copy1:
+    for item in row:
+      if item == "1":
+        print(item,end=' ')
+      elif item == "0":
+        print(colored(item,'yellow'),end=' ')
+      elif item == "0 ":
+        print("\n"+colored(item,'red'),end=' ')
+      else:
+        print(colored(item,'blue'),end=' ')
+    print()
+  print()
+
+def createTempList(copy2):
+  temp = []
+  for copy_value in copy2:
+    temp.append(copy_value)
+  return temp
